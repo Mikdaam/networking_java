6b6c8d69ccd33f754c9e24cb881fe3f19d643317
Mikdaam <59888115+Mikdaam@users.noreply.github.com>
2023-04-19 11:38:43 +0200

    Fault tolerant udp (#4)
    * adding all the necessary file for this lab.
    
    * add the first exercice of Lab4 and add i in 'ini file.
    
    * out file
    
    * using the same method for flip
    
    * burst one and result

---
+++ Fault_tolerance_udp/src/main/java/fr/network/fault_tolerant_udp/ClientUpperCaseUDPTimeout.java
@@ -0,0 +1,71 @@
+package fr.network.fault_tolerant_udp;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousCloseException;
+import java.nio.channels.DatagramChannel;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+public class ClientUpperCaseUDPTimeout {
+    private static final Logger logger = Logger.getLogger(ClientUpperCaseUDPTimeout.class.getName());
+    public static final int BUFFER_SIZE = 1024;
+
+    private static void usage() {
+        System.out.println("Usage : NetcatUDP host port charset");
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        if (args.length != 3) {
+            usage();
+            return;
+        }
+
+        var server = new InetSocketAddress(args[0], Integer.parseInt(args[1]));
+        var cs = Charset.forName(args[2]);
+        var buffer = ByteBuffer.allocate(BUFFER_SIZE);
+        var messageQueue = new ArrayBlockingQueue<String>(10);
+
+        try(var dataChan = DatagramChannel.open()) {
+            dataChan.bind(null); // Choose a random port.
+
+            Thread.ofPlatform().start(() -> {
+                while (true) {
+                    try {
+                        var exp = dataChan.receive(buffer);
+                        buffer.flip();
+                        System.out.println("Received " + buffer.remaining() + " bytes from " + exp);
+                        messageQueue.put(cs.decode(buffer).toString());
+                        buffer.clear();
+                    } catch (InterruptedException e) {
+                        throw new AssertionError();
+                    } catch (AsynchronousCloseException e) {
+                        logger.info("Closing listener");
+                        break;
+                    } catch (IOException e) {
+                        throw new AssertionError(e);
+                    }
+                }
+            });
+
+            try (var scanner = new Scanner(System.in)) {
+                while (scanner.hasNextLine()) {
+                    var line = scanner.nextLine();
+                    dataChan.send(cs.encode(line), server);
+                    var msg = messageQueue.poll(1_000, TimeUnit.MILLISECONDS);
+                    if (msg == null) {
+                        logger.warning("Server did not respond in time");
+                        break;
+                    }
+                    System.out.println("String: " +  msg);
+                }
+            }
+        }
+        logger.info("End Of Program");
+    }
+}
