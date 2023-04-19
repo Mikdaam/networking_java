04dd5e81d69df7b97b6b7637a96bca26bfabd722
Mick <bmikdaam@gmail.com>
2023-04-19 11:12:04 +0200

    exercice 1 and 3

---
+++ blocking_tcp_server/src/main/java/fr/network/blocking_tcp_server/exo3/FixedPrestartedLongSumServer.java
@@ -0,0 +1,128 @@
+package fr.network.blocking_tcp_server.exo3;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class FixedPrestartedLongSumServer {
+    private static final Logger logger = Logger.getLogger(FixedPrestartedLongSumServer.class.getName());
+    private static final int BUFFER_SIZE = 1024;
+    private final ServerSocketChannel serverSocketChannel;
+    private final int maxClient;
+
+
+    public FixedPrestartedLongSumServer(int port, int maxClient) throws IOException {
+        serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.bind(new InetSocketAddress(port));
+        logger.info(this.getClass().getName() + " starts on port " + port);
+        this.maxClient = maxClient;
+    }
+
+    /**
+     * Iterative server main loop
+     *
+     * @throws IOException
+     */
+
+    public void launch() throws IOException {
+        logger.info("Server started");
+
+        for (int i = 0; i < maxClient; i++) {
+            Thread.ofPlatform().start(() -> {
+                try {
+                    while (!Thread.interrupted()) {
+                        SocketChannel client = serverSocketChannel.accept();
+                        try {
+                            logger.info("Connection accepted from " + client.getRemoteAddress());
+                            serve(client);
+                        } catch (IOException ioe) {
+                            logger.log(Level.SEVERE, "Connection terminated with client by IOException", ioe.getCause());
+                        } finally {
+                            silentlyClose(client);
+                        }
+                    }
+                } catch (IOException e) {
+                    logger.log(Level.SEVERE, "Accept can accept anymore");
+                    return;
+                }
+            });
+        }
+    }
+
+    /**
+     * Treat the connection sc applying the protocol. All IOException are thrown
+     *
+     * @param sc
+     * @throws IOException
+     */
+    private void serve(SocketChannel sc) throws IOException {
+        var bufferNbOps = ByteBuffer.allocate(Integer.BYTES);
+        long sum = 0;
+
+        while (true) {
+            bufferNbOps.clear();
+            if (!readFully(sc, bufferNbOps)) {
+                logger.info("Wrong packet format. Close connection");
+                return;
+            }
+            bufferNbOps.flip();
+            int nbOps = bufferNbOps.getInt();
+            if (nbOps < 0) {
+                logger.info("Wrong packet format. Close connection");
+                return;
+            }
+
+            var longs = ByteBuffer.allocate(nbOps * Long.BYTES);
+            if (!readFully(sc, longs)) {
+                logger.info("Wrong packet format. Close connection");
+                return;
+            }
+
+            while (longs.hasRemaining()) {
+                sum += longs.getLong();
+            }
+
+            longs.clear();
+            longs.putLong(sum);
+            longs.flip();
+
+            sc.write(longs);
+        }
+    }
+
+    /**
+     * Close a SocketChannel while ignoring IOExecption
+     *
+     * @param sc
+     */
+
+    private void silentlyClose(Closeable sc) {
+        if (sc != null) {
+            try {
+                sc.close();
+            } catch (IOException e) {
+                // Do nothing
+            }
+        }
+    }
+
+    static boolean readFully(SocketChannel sc, ByteBuffer buffer) throws IOException {
+        while (buffer.hasRemaining()) {
+            if (sc.read(buffer) == -1) {
+                logger.info("Input stream closed");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        var server = new FixedPrestartedLongSumServer(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        server.launch();
+    }
+}
