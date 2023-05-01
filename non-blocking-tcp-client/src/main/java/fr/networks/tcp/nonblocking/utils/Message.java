98dcb0f689b0dbf5ae6df464d0abfdac48b665d1
Mick <bmikdaam@gmail.com>
2023-05-01 18:18:31 +0200

    Neccesary files for ClientChat

---
+++ non-blocking-tcp-client/src/main/java/fr/networks/tcp/nonblocking/utils/Message.java
@@ -0,0 +1,14 @@
+package fr.networks.tcp.nonblocking.utils;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+public record Message(String login, String msg) {
+    public ByteBuffer encode() {
+        var bb = ByteBuffer.allocate(1024);
+        var loginBytes = StandardCharsets.UTF_8.encode(login);
+        var msgBytes = StandardCharsets.UTF_8.encode(msg);
+        return bb.putInt(loginBytes.remaining()).put(loginBytes)
+                .putInt(msgBytes.remaining()).put(msgBytes);
+    }
+}
